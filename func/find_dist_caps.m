4fcc750a278627bc43405e1856b718f48ca9b513
yzhaoinuw <yzhaoinuw@gmail.com>
2024-08-16 19:07:57 -0400

    use the updated find_dist_caps.m made by Kim.

--- func/find_dist_caps.m
+++ func/find_dist_caps.m
@@ -40,19 +40,23 @@ end
 if isempty(seg) || ~exist('seg','var')
     stats=regionprops(bw_caps,'Centroid');
     dist_caps=((stats(1).Centroid(1)-stats(2).Centroid(1)).^2+(stats(1).Centroid(2)-stats(2).Centroid(2)).^2).^0.5;
+    dist_caps=dist_caps-3; % this is b/c bw_caps, which is 3 pixels wide, is outside of the segmented region. So it's off by 1.5 pixels on either end, or 3 pixels total
 
     % visualize
     imagei({bw_caps,bw_caps,bw_caps},{mask bw_caps zeros(size(bw_caps))}), axis equal, 
-    hold on, plot([stats(1).Centroid(1) stats(2).Centroid(1)],[stats(1).Centroid(2) stats(2).Centroid(2)],'y','LineWidth',4)
+    hold on, plot([stats(1).Centroid(1) stats(2).Centroid(1)]-1,[stats(1).Centroid(2) stats(2).Centroid(2)]-1,'y','LineWidth',4)
 
 else
-    caps_trim=bw_caps & imdilate(imbinarize(mean(seg,3),0.5),strel('disk',1)); 
-    stats=regionprops(caps_trim,'Centroid');
-    dist_caps=((stats(1).Centroid(1)-stats(2).Centroid(1)).^2+(stats(1).Centroid(2)-stats(2).Centroid(2)).^2).^0.5;
-        
-    % visualize
-    imagei({mean(seg,3) mean(seg,3) mean(seg,3)},{mask bw_caps caps_trim}), axis equal, 
-    hold on, plot([stats(1).Centroid(1) stats(2).Centroid(1)],[stats(1).Centroid(2) stats(2).Centroid(2)],'y','LineWidth',4)
+     caps_trim=bw_caps & imdilate(imbinarize(mean(seg,3),0.5),strel('disk',1)); 
+     stats=regionprops(caps_trim,'Centroid');
+     dist_caps=((stats(1).Centroid(1)-stats(2).Centroid(1)).^2+(stats(1).Centroid(2)-stats(2).Centroid(2)).^2).^0.5; 
+     dist_caps=dist_caps-1; % this is b/c the line goes from one pixel outside of the segmented region on either end. (This is because we dilated seg by 1 pixel.) But it goes from the center of the pixel, so it's only off by half a pixel on each end. You could make the trimmed caps be the intersection of a dilated mask and the non-dilated segmented region, but then you'd have to add a pixel because the centroid goes from the center of the pixel. 
+     
+     % visualize
+     imagei({mean(seg,3) mean(seg,3) mean(seg,3)},{mask bw_caps caps_trim}), axis equal, 
+     hold on, plot([stats(1).Centroid(1) stats(2).Centroid(1)]-1,[stats(1).Centroid(2) stats(2).Centroid(2)]-1,'y','LineWidth',4) % subtract 1 from the x and y coordinates for the visualization b/c the indexing for imagei starts from 0
+
+
 end
 
 %% old ideas
@@ -60,3 +64,31 @@ end
 % [xctr,yctr]=FindCntrPts(seg(:,:,1));
 % [line_x, line_y, xc, yc] = FindSpanningLinesOnly(xctr,yctr,numlines,numpts,linelength,polyord);
 end
+
+%% code graveyard
+ % Modified version -----------------
+    % caps_trim=bw_caps & imdilate(imbinarize(mean(seg,3),0.5),strel('disk',1)); 
+    % stats=regionprops(caps_trim,'Centroid');
+    % centroid1=[stats(1).Centroid(1), stats(1).Centroid(2)];
+    % centroid2=[stats(2).Centroid(1), stats(2).Centroid(2)];
+    
+    % Finding the direction vector
+    % dir_vec=centroid2-centroid1;
+
+    % Normalizing the direction vector
+    % unit_vec=dir_vec/norm(dir_vec);
+
+    % Extension vector (extending by 1 pixel)
+    % extension=unit_vec*1;
+
+    % New endpoints
+    % end_point1=centroid1-extension;
+    % end_point2=centroid2;
+
+    % dist_caps=((end_point1(1)-end_point2(1))^2+(end_point1(2)-end_point2(2))^2)^0.5;
+
+    % visualize
+    % imagei({mean(seg,3) mean(seg,3) mean(seg,3)},{mask bw_caps caps_trim}), axis equal, 
+    % hold on, 
+    % plot([end_point1(1),end_point2(1)],[end_point1(2),end_point2(2)],'y','LineWidth',4)
+    % hold off
